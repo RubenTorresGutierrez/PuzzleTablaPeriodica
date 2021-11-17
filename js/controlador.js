9d1baaf35fa3d393a8855463e4725e1dcce71c53
Abelmf98 <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
2021-11-17 09:41:21 +0100

    Drag and drop

---
+++ js/controlador.js
@@ -0,0 +1,58 @@
+/** 
+ *  @file Controlador | Puzzle Tabla Periódica
+ *  @description Controlador principal del juego
+ *  @version 1.0.0
+ *  @author Abel Mansilla Felipe <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Juanjo Carrasco Rodríguez <jcarrascorodriguez.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Alejandro Moreno Camacho <amorenocamacho.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Rubén Torres Gutiérrez <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
+ *  @license LGPL-3.0-or-later
+ */
+
+'use strict'
+
+// IMPORTACIONES
+import {Vista} from './vista.js';
+import {Modelo} from './modelo.js';
+
+/**
+ * Controlado principal del juego
+ */
+class Controlador{
+
+    constructor(){
+
+        // OBJETOS
+        this.vista = new Vista();
+        this.modelo = new Modelo();
+
+        // HTML
+        this.main = null;
+
+        // INICIAR
+        window.onload = this.iniciar.bind(this);
+
+    }
+
+    /**
+     * Realiza la ejecución principal del programa
+     */
+    iniciar(){
+
+        // ATRIBUTOS
+        // Atributos controlador
+        this.main = document.getElementsByTagName('main')[0];
+
+        // Atributos vista
+        this.vista.contenedorElementos = document.getElementById('elementos');
+
+        // COMIENZO DEL JUEGO
+        // Creación de elementos
+        window.setInterval(this.vista.crearElementos.bind(this.vista), 2000);
+        window.setInterval(this.vista.movimiento.bind(this.vista), 100);
+
+    }
+
+}
+
+let app = new Controlador();
---
+++ js/vista.js
@@ -0,0 +1,81 @@
+/**
+ *  @file Vista | Puzzle Tabla Periódica
+ *  @description Clase vista que sirve para manejar el diseño de la web
+ *  @version 1.0.0
+ *  @author Abel Mansilla Felipe <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Juanjo Carrasco Rodríguez <jcarrascorodriguez.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Alejandro Moreno Camacho <amorenocamacho.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Rubén Torres Gutiérrez <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
+ *  @license LGPL-3.0-or-later
+ */
+
+ 'use strict'
+
+ // IMPORTACIONES
+ import {Sprite} from './sprite.js';
+
+ /**
+  * Clase vista que sirve para manejar el diseño de la web
+  */
+export class Vista{
+
+    constructor(){
+
+        this.contenedorElementos = null;
+        this.imagen = [];
+        this.sprites = []
+
+    }
+
+    /**
+     * Crea un objeto de la clase Sprite() dentro del array sprites[]
+     */
+    crearElementos(){
+
+        this.sprites.push(new Sprite(this.contenedorElementos))
+
+    }
+
+    /**
+     * Llama al método mover() de todos los objetos de la clase Sprite existentes
+     */
+    movimiento(){
+
+        // Llamar a la función para mover el muñeco
+        for(let i = 0; i<this.sprites.length;i++)
+            this.sprites[i].mover();
+
+    }
+
+}
+let img=document.getElementsByTagName("img")[0];
+img.onmousedown=draganddrop;
+function draganddrop(){
+    /*  Para arrastar el elemento sin errores. */
+    img.ondragstart=function(){
+        return false;
+    }
+
+    img.style.position="absolute";
+    img.style.zIndex="200";
+
+    /*Posicion respecto al body.*/
+    document.body.append(img);
+
+    /*Mover la imagen.*/
+    mover(event.pageX, event.pageY);
+    function mover(x, y){
+        img.style.left=x-img.offsetWidth/2+"px";
+        img.style.top=y-img.offsetHeight/2+"px";
+    }
+    function movimiento(e){
+        mover(event.pageX, event.pageY);
+    }
+    document.addEventListener('mousemove', movimiento);
+
+    /*Eliminamos el evento.*/
+    img.onmouseup=function(){
+        document.removeEventListener('mousemove', movimiento);
+        img.onmouseup=null;
+    }
+}
