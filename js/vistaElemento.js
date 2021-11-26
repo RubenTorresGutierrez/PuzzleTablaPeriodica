85c741cbc92e28034190911822e6f8777b3c277b
Abelmf98 <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
2021-11-26 11:43:23 +0100

    Carrousel

---
+++ js/vistaElemento.js
@@ -0,0 +1,33 @@
+/**
+ *  @file VistaElemento | Puzzle Tabla Periódica
+ *  @description Clase que sirve para interactuar con los elementos de la tabla periódica
+ *  @version 1.0.0
+ *  @author Abel Mansilla Felipe <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Juanjo Carrasco Rodríguez <jcarrascorodriguez.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Alejandro Moreno Camacho <amorenocamacho.guadalupe@alumnado.fundacionloyola.net>
+ *  @author Rubén Torres Gutiérrez <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
+ *  @license LGPL-3.0-or-later
+ */
+
+ 'use strict'
+ 
+/**
+ * Clase que sirve para crear y mover los elementos
+ * @param div {HTMLElement} Contenedor donde se almacenan y se mueven los elementos
+ * @param velocidad {int} Pixeles que se moverá hacia la derecha el elemento
+ */
+ export class VistaElemento{
+
+    constructor(div, velocidad){
+
+        // Contenedor donde se almacenan y se mueven los muñecos
+        this.div = div;
+
+        // Posición left del muñeco
+        this.x = 30;
+        // Velocidad a la que avanza el muñeco
+        this.vX = velocidad;
+
+    }
+
+ }
